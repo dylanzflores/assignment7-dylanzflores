210b8ddb6d80465989370f4d9c85dfe7bff33524
dylanzflores <dyl.flores@protonmail.com>
2025-09-24 01:32:35 +0000

    Update hello.c to print GitHub username

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world from dylanzflores\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world from dylanzflores\n");
 }
 
 module_init(hello_init);
